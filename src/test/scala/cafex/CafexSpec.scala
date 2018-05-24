901460566245362f98d9d361e0de209e8b738806
VasicJernej <jernej.vasic@gmail.com>
2018-05-24 21:58:28 +0100

    CAF1: First part of the task
    Creat a class that computes the correct total
    for a list of items bought.
    
    - Add basic project files and folders
    - Add basic tests

--- src/test/scala/cafex/CafexSpec.scala
+++ src/test/scala/cafex/CafexSpec.scala
@@ -5,21 +5,21 @@ import org.scalatest.BeforeAndAfter
 
 class CafexSpec extends FunSuite with BeforeAndAfter {
 
-	var cafex: Cafex = _
-	before {
-		cafex = new Cafex
-	}
+  var cafex: Cafex = _
+  before {
+    cafex = new Cafex
+  }
 
-	test("non existent food returns 0") {
-		assert(cafex.total(List("dummy")) == 0)
-	}
+  test("non existent food returns 0") {
+    assert(cafex.total(List("dummy")) == 0)
+  }
 
-	test("filters bad values correctly") {
-		val orders = List("Cola", "Coffee", "Cheese Sandwich", "asd", "Coffee2")
-		assert(cafex.total(orders) == 3.5)
-	}
+  test("filters bad values correctly") {
+    val orders = List("Cola", "Coffee", "Cheese Sandwich", "asd", "Coffee2")
+    assert(cafex.total(orders) == 3.5)
+  }
 
-	test("empty list returns 0") {
-		assert(cafex.total(List()) == 0)
-	}
+  test("empty list returns 0") {
+    assert(cafex.total(List()) == 0)
+  }
 }
