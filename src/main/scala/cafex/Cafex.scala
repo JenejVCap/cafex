c3e2b30bd2a341890eb2abfc3ea51311fc41b3ed
VasicJernej <jernej.vasic@gmail.com>
2018-05-25 08:56:04 +0100

    CAF2: Second part of the task
    Add support for service charges and refactor tests

--- src/main/scala/cafex/Cafex.scala
+++ src/main/scala/cafex/Cafex.scala
@@ -1,22 +1,92 @@
 package cafex
 
 import scala.math.BigDecimal
+import scala.math.BigDecimal.RoundingMode
 
 class Cafex {
 
+  val menu: Map[String, Product] = Map(
+    "Cola" -> Product(Constants.drink, Constants.cold, 0.5),
+    "Coffee" -> Product(Constants.drink, Constants.hot, 1),
+    "Cheese Sandwich" -> Product(Constants.food, Constants.cold, 2),
+    "Steak Sandwich" -> Product(Constants.food, Constants.hot, 4.5)
+  )
+
   /**
-   * Total money to pay for the order
+   * Total money to pay for the order + service charge
    *
    * @param items A list of drinks and foods
    */
-  def total(items: List[String]): BigDecimal = items.map { menu.get(_) }.filter {
-    x => x.isDefined && (x.get > 0)
-  }.map { _.get }.sum
+  def total(items: List[String]) = {
+    val ordered: List[Product] = items.map { menu.get(_) }.filter {
+      x => x.isDefined
+    }.map { _.get }
 
-  val menu: Map[String, BigDecimal] = Map(
-    "Cola" -> 0.5,
-    "Coffee" -> 1,
-    "Cheese Sandwich" -> 2,
-    "Steak Sandwich" -> 4.5
-  )
+    def serviceCharge(products: List[Product], charge: Charges): Int = products match {
+      case Nil => Charges.getCharge(charge)
+      case x => if (x.head.itemType == Constants.food && x.head.temperatureType == Constants.hot) {
+        serviceCharge(x.tail, Charges(charge.onlyDrinks, charge.hasFood, true))
+      } else if (x.head.itemType == Constants.food) {
+        serviceCharge(x.tail, Charges(charge.onlyDrinks, true, charge.hasHotFood))
+      } else {
+        serviceCharge(x.tail, Charges(true, charge.hasFood, charge.hasHotFood))
+      }
+    }
+
+    val chargePct: Int = serviceCharge(ordered, Charges(false, false, false))
+    val totalWithoutCharge = items.map { menu.get(_) }.filter {
+      x => x.isDefined && (x.getOrElse(Product("", "", 0)).cost > 0)
+    }.map { _.get.cost }.sum
+
+    def totalCharge(orderCost: BigDecimal, charge: Int): BigDecimal = {
+      val tcharge = (orderCost * (charge * 0.01)).setScale(2, RoundingMode.HALF_EVEN)
+      if (tcharge > Constants.maxCharge) Constants.maxCharge else tcharge
+    }
+
+    totalWithoutCharge + totalCharge(totalWithoutCharge, chargePct)
+  }
+}
+
+/**
+ * Description of a food/drink in the menu
+ */
+case class Product(
+  itemType: String,
+  temperatureType: String,
+  cost: BigDecimal
+)
+
+object Constants {
+
+  val cold = "cold"
+  val hot = "hot"
+  val food = "food"
+  val drink = "drink"
+
+  val onlyDrinksCharge = 0
+  val foodCharge = 10
+  val hotFoodCharge = 20
+
+  val maxCharge = 20
+
+}
+
+/**
+ * Possible charges.
+ *
+ * The values are mutually exclusive
+ */
+case class Charges(
+  onlyDrinks: Boolean,
+  hasFood: Boolean,
+  hasHotFood: Boolean
+)
+
+object Charges {
+
+  def getCharge(ss: Charges): Int = {
+    if (ss.hasHotFood) Constants.hotFoodCharge
+    else if (!ss.hasHotFood && ss.hasFood) Constants.foodCharge
+    else Constants.onlyDrinksCharge
+  }
 }
--- src/test/scala/cafex/CafexSpec.scala
+++ src/test/scala/cafex/CafexSpec.scala
@@ -10,16 +10,33 @@ class CafexSpec extends FunSuite with BeforeAndAfter {
     cafex = new Cafex
   }
 
+  test("empty list returns 0") {
+    assert(cafex.total(List()) == 0)
+  }
+
   test("non existent food returns 0") {
     assert(cafex.total(List("dummy")) == 0)
   }
 
   test("filters bad values correctly") {
-    val orders = List("Cola", "Coffee", "Cheese Sandwich", "asd", "Coffee2")
-    assert(cafex.total(orders) == 3.5)
+    val orders = List("Cola", "Coffee", "Cheese Sandwich", "dummy", "Coffee2")
+    assert(cafex.total(orders) == 3.85)
   }
 
-  test("empty list returns 0") {
-    assert(cafex.total(List()) == 0)
+  test("only drinks, no charge") {
+    assert(cafex.total(List("Cola", "Coffee")) == 1.5)
+  }
+
+  test("cold food adds 10 pct") {
+    assert(cafex.total(List("Cheese Sandwich")) == 2.2)
+  }
+
+  test("hot food adds 20 pct") {
+    assert(cafex.total(List("Steak Sandwich")) == 5.4)
+  }
+
+  test("no more than 20 units to be added as service charge") {
+    val longList = List.fill(100)("Steak Sandwich")
+    assert(cafex.total(longList) == 470)
   }
 }
