8f2750c90337aaca22bdd940dc059405cb4ab61a
Jernej Vasic <jernej.vasic@digital.hmrc.gov.uk>
2019-04-17 17:05:53 +0100

    CAF-3: Refactor challenge

--- src/main/scala/cafex/Cafex.scala
+++ src/main/scala/cafex/Cafex.scala
@@ -1,92 +1,59 @@
 package cafex
 
-import scala.math.BigDecimal
-import scala.math.BigDecimal.RoundingMode
+object Cafex {
+  def main(args: Array[String]): Unit = {
+    println("Just run the tests with sbt test!")
+  }
 
-class Cafex {
+  case class Item(name: String, cost: Double, state: State, itemType: ItemType)
 
-  val menu: Map[String, Product] = Map(
-    "Cola" -> Product(Constants.drink, Constants.cold, 0.5),
-    "Coffee" -> Product(Constants.drink, Constants.hot, 1),
-    "Cheese Sandwich" -> Product(Constants.food, Constants.cold, 2),
-    "Steak Sandwich" -> Product(Constants.food, Constants.hot, 4.5)
+  sealed trait State
+
+  case object Hot extends State
+
+  case object Cold extends State
+
+  sealed trait ItemType
+
+  case object Food extends ItemType
+
+  case object Drink extends ItemType
+
+  val menu: List[Item] = List(
+    Item("Cola", 0.50, Cold, Drink),
+    Item("Coffee", 1.00, Hot, Drink),
+    Item("Cheese Sandwich", 2.00, Cold, Food),
+    Item("Steak Sandwich", 4.50, Hot, Food)
   )
 
-  /**
-   * Total money to pay for the order + service charge
-   *
-   * @param items A list of drinks and foods
-   */
-  def total(items: List[String]) = {
-    val ordered: List[Product] = items.map { menu.get(_) }.filter {
-      x => x.isDefined
-    }.map { _.get }
 
-    def serviceCharge(products: List[Product], charge: Charges): Int = products match {
-      case Nil => Charges.getCharge(charge)
-      case x => if (x.head.itemType == Constants.food && x.head.temperatureType == Constants.hot) {
-        serviceCharge(x.tail, Charges(charge.onlyDrinks, charge.hasFood, true))
-      } else if (x.head.itemType == Constants.food) {
-        serviceCharge(x.tail, Charges(charge.onlyDrinks, true, charge.hasHotFood))
-      } else {
-        serviceCharge(x.tail, Charges(true, charge.hasFood, charge.hasHotFood))
-      }
+
+  def totalBillWithServiceCharge(items: List[String]): Double = {
+    def totalBill(items: List[String], total: Double = 0.0): Double = items match {
+      case Nil => total
+      case h::t =>
+        val cost = menu.collectFirst {
+          case item if item.name == h => item.cost
+        }.getOrElse(0.0)
+        totalBill(t, total + cost)
     }
-
-    val chargePct: Int = serviceCharge(ordered, Charges(false, false, false))
-    val totalWithoutCharge = items.map { menu.get(_) }.filter {
-      x => x.isDefined && (x.getOrElse(Product("", "", 0)).cost > 0)
-    }.map { _.get.cost }.sum
-
-    def totalCharge(orderCost: BigDecimal, charge: Int): BigDecimal = {
-      val tcharge = (orderCost * (charge * 0.01)).setScale(2, RoundingMode.HALF_EVEN)
-      if (tcharge > Constants.maxCharge) Constants.maxCharge else tcharge
+    val totalNoSC: Double = totalBill(items)
+    val itemsAvailable: List[Item] = items.flatMap { it => menu.collectFirst {
+      case item if item.name == it => item
+    }}
+    val hasHotFood: Option[Boolean] = itemsAvailable collectFirst {
+      case item if item.state == Hot && item.itemType == Food => true
     }
-
-    totalWithoutCharge + totalCharge(totalWithoutCharge, chargePct)
-  }
-}
-
-/**
- * Description of a food/drink in the menu
- */
-case class Product(
-  itemType: String,
-  temperatureType: String,
-  cost: BigDecimal
-)
-
-object Constants {
-
-  val cold = "cold"
-  val hot = "hot"
-  val food = "food"
-  val drink = "drink"
-
-  val onlyDrinksCharge = 0
-  val foodCharge = 10
-  val hotFoodCharge = 20
-
-  val maxCharge = 20
-
-}
-
-/**
- * Possible charges.
- *
- * The values are mutually exclusive
- */
-case class Charges(
-  onlyDrinks: Boolean,
-  hasFood: Boolean,
-  hasHotFood: Boolean
-)
-
-object Charges {
-
-  def getCharge(ss: Charges): Int = {
-    if (ss.hasHotFood) Constants.hotFoodCharge
-    else if (!ss.hasHotFood && ss.hasFood) Constants.foodCharge
-    else Constants.onlyDrinksCharge
+    val hasFood: Option[Boolean] = itemsAvailable collectFirst {
+      case item if item.itemType == Food => true
+    }
+    val serviceCharge: Double = (hasHotFood, hasFood) match {
+      case (Some(_), Some(_)) => 0.20
+      case (None, Some(_)) => 0.10
+      case _ => 0.0
+    }
+    val totalWithServiceCharge = if (totalNoSC * serviceCharge > 20) totalNoSC + 20
+      else totalNoSC + (totalNoSC * serviceCharge)
+    totalWithServiceCharge
   }
 }
