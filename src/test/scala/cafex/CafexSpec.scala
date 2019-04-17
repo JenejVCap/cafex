502c5d954f6d28781776d118db60da566c293bcc
Jernej Vasic <jernej.vasic@digital.hmrc.gov.uk>
2019-04-17 17:05:53 +0100

    CAF-3: Refactor challenge

--- src/test/scala/cafex/CafexSpec.scala
+++ src/test/scala/cafex/CafexSpec.scala
@@ -1,42 +1,36 @@
 package cafex
 
 import org.scalatest.FunSuite
-import org.scalatest.BeforeAndAfter
 
-class CafexSpec extends FunSuite with BeforeAndAfter {
-
-  var cafex: Cafex = _
-  before {
-    cafex = new Cafex
-  }
+class CafexSpec extends FunSuite {
 
   test("empty list returns 0") {
-    assert(cafex.total(List()) == 0)
+    assert(Cafex.totalBillWithServiceCharge(List()) == 0)
   }
 
   test("non existent food returns 0") {
-    assert(cafex.total(List("dummy")) == 0)
+    assert(Cafex.totalBillWithServiceCharge(List("dummy")) == 0)
   }
 
   test("filters bad values correctly") {
     val orders = List("Cola", "Coffee", "Cheese Sandwich", "dummy", "Coffee2")
-    assert(cafex.total(orders) == 3.85)
+    assert(Cafex.totalBillWithServiceCharge(orders) == 3.85)
   }
 
   test("only drinks, no charge") {
-    assert(cafex.total(List("Cola", "Coffee")) == 1.5)
+    assert(Cafex.totalBillWithServiceCharge(List("Cola", "Coffee")) == 1.5)
   }
 
   test("cold food adds 10 pct") {
-    assert(cafex.total(List("Cheese Sandwich")) == 2.2)
+    assert(Cafex.totalBillWithServiceCharge(List("Cheese Sandwich")) == 2.2)
   }
 
   test("hot food adds 20 pct") {
-    assert(cafex.total(List("Steak Sandwich")) == 5.4)
+    assert(Cafex.totalBillWithServiceCharge(List("Steak Sandwich")) == 5.4)
   }
 
   test("no more than 20 units to be added as service charge") {
     val longList = List.fill(100)("Steak Sandwich")
-    assert(cafex.total(longList) == 470)
+    assert(Cafex.totalBillWithServiceCharge(longList) == 470)
   }
 }
